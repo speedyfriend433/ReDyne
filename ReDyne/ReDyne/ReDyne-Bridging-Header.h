e53e135642d4bbe2364b062e868941bcf989825d
Speedyfriend67 <speedyfriend13@gmail.com>
2025-10-07 17:22:56 +0900

    Update ReDyne-Bridging-Header.h

--- ReDyne/ReDyne/ReDyne-Bridging-Header.h
+++ ReDyne/ReDyne/ReDyne-Bridging-Header.h
@@ -1,23 +1,12 @@
-// File: ReDyne/ReDyne-Bridging-Header.h - Objective-C to Swift bridging header
-// Exposes Objective-C and C headers to Swift code
-
 #ifndef ReDyne_Bridging_Header_h
 #define ReDyne_Bridging_Header_h
 
-// Import Objective-C headers
 #import "AppDelegate.h"
 #import "SceneDelegate.h"
-
-// Import Model headers
 #import "DecompiledOutput.h"
-
-// Import Service headers
 #import "BinaryParserService.h"
 #import "DisassemblerService.h"
 #import "ObjCParserBridge.h"
-
-// Import C headers (wrapped by Objective-C services)
-// Note: Direct C usage is handled through Objective-C wrappers
 #import "StringExtractor.h"
 #import "MachOHeader.h"
 #import "SymbolTable.h"
@@ -27,6 +16,9 @@
 #import "ObjCParser.h"
 #import "DyldInfo.h"
 #import "CodeSignature.h"
+#import "EnhancedFilePicker.h"
+#import "PseudocodeGenerator.h"
+#import "ARM64InstructionDecoder.h"
 
-#endif /* ReDyne_Bridging_Header_h */
+#endif
 
