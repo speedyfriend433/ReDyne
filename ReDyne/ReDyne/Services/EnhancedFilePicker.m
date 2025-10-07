438bea47b0a7779bca4f3ba6f1bc1991d08fed0a
Speedyfriend67 <speedyfriend13@gmail.com>
2025-10-07 17:20:15 +0900

    Create EnhancedFilePicker.m

---
+++ ReDyne/ReDyne/Services/EnhancedFilePicker.m
@@ -0,0 +1,74 @@
+#import "EnhancedFilePicker.h"
+#import "LCSharedUtils.h"
+#import "UIKitPrivate.h"
+#import "utils.h"
+#import "FoundationPrivate.h"
+
+static BOOL enhancedFilePickerActive = NO;
+static void NSFMGuestHooksInit(void);
+
+@implementation EnhancedFilePicker
+
++ (void)enable {
+    if (enhancedFilePickerActive) {
+        return;
+    }
+
+    enhancedFilePickerActive = YES;
+    NSFMGuestHooksInit();
+}
+
++ (void)disable {
+    if (!enhancedFilePickerActive) {
+        return;
+    }
+
+    enhancedFilePickerActive = NO;
+}
+
++ (BOOL)isActive {
+    return enhancedFilePickerActive;
+}
+
+@end
+
+BOOL fixFilePicker;
+__attribute__((constructor))
+static void NSFMGuestHooksInit() {
+    if (!enhancedFilePickerActive) {
+        return;
+    }
+
+    fixFilePicker = YES;
+
+    /*
+    // Hook document picker initialization
+    swizzle(UIDocumentPickerViewController.class,
+            @selector(initForOpeningContentTypes:asCopy:),
+            @selector(hook_initForOpeningContentTypes:asCopy:));
+
+    swizzle(UIDocumentPickerViewController.class,
+            @selector(initWithDocumentTypes:inMode:),
+            @selector(hook_initWithDocumentTypes:inMode:));
+
+    swizzle(UIDocumentBrowserViewController.class,
+            @selector(initForOpeningContentTypes:),
+            @selector(hook_initForOpeningContentTypes));
+
+    swizzleClassMethod(UTType.class,
+                      @selector(typeWithIdentifier:),
+                      @selector(hook_typeWithIdentifier:));
+
+    if (fixFilePicker) {
+        swizzle(NSURL.class,
+                @selector(startAccessingSecurityScopedResource),
+                @selector(hook_startAccessingSecurityScopedResource));
+
+        swizzle(UIDocumentPickerViewController.class,
+                @selector(setAllowsMultipleSelection:),
+                @selector(hook_setAllowsMultipleSelection:));
+    }
+    */
+    // swizzle(DOCConfiguration.class, @selector(setHostIdentifier:), @selector(hook_setHostIdentifier:));
+}
+
